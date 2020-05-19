48b2ef8c4d0923d2749bb00fd4e92747bbb4f457
Stuart Gill <stuart.a.gill@gmail.com>
2020-05-19 10:13:45 -0700

    finish bouncer

--- bouncer.py
+++ bouncer.py
@@ -1,10 +1,15 @@
 
 age = input("how old are you? ")
-age = int(age)
 
-if age >=21:
-  print("ok normal entry")
-elif age < 18:
-  print("too young, sorry!")
+if age:
+  age = int(age)
+  if age >=21:
+    print("ok normal entry")
+  elif age < 18:
+    print("too young, sorry!")
+  else:
+    print("you can come in but no drinks!")
 else:
-  print("you can come in but no drinks!")
+  print('invalid input')
+
+# doesn't handle improper input... comes in as str, convert to int-- how to determine if str is a str representation of an int without trying to conver to int, which causes an error if str doesn't represent an int?

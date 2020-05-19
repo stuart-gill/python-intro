77fd431337408d4082b985e8a6473d22cd2fb619
Stuart Gill <stuart.a.gill@gmail.com>
2020-05-19 11:30:26 -0700

    move tie conditional to top

--- rps.py
+++ rps.py
@@ -8,29 +8,26 @@ player1 = input()
 print('Enter Player 2 Choice:')
 player2 = input()
 
-if (player1 == 'rock'):
+if player1==player2:
+  print("It's a tie!!")
+
+elif (player1 == 'rock'):
   if (player2 == 'scissors'):
     print('Player 1 Win!')
   elif (player2 == 'paper'):
     print('Player 2 Win!')
-  else:
-    print('Tie!!')
 
 elif (player1 == 'scissors'):
   if (player2 == 'paper'):
     print('Player 1 Win!')
   elif (player2 == 'rock'):
     print('Player 2 Win!')
-  else:
-    print('Tie!!')
 
 elif (player1 == 'paper'):
   if (player2 == 'rock'):
     print('Player 1 Win!')
   elif (player2 == 'scissors'):
     print('Player 2 Win!')
-  else:
-    print('Tie!!')
 
 else:
   print('input error, sorry!')
